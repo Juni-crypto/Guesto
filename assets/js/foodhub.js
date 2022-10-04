3ee52b05ed69e3bb84f26a296521b1adf559f012
juni-crypto <chander.cia005@gmail.com>
2022-10-05 00:30:56 +0530

    Ui-changes

--- assets/js/foodhub.js
+++ assets/js/foodhub.js
@@ -4,12 +4,9 @@
 // navbar variables
 const nav = document.querySelector('.navbar-nav');
 const navLinks = document.querySelectorAll('.nav-link');
-const cartToggleBtn = document.querySelector('.shopping-cart-btn');
 const navToggleBtn = document.querySelector('.menu-toggle-btn');
-const shoppingCart = document.querySelector('.cart-box');
 
 
-const fullmenu = document.getElementById('fullmenu');
 
 // nav toggle function
 const navToggleFunc = function () {
@@ -18,33 +15,17 @@ const navToggleFunc = function () {
 }
 
 // shopping cart toggle function
-const cartToggleFunc = function () { shoppingCart.classList.toggle('active') }
 
-fullmenu.addEventListener('click', function () {
 
-  console.log('clicked');
-  window.open('https://www.flipbookpdf.net/web/site/041f1a883dac6d1f9674fb42ca67e6ba5cced3e5202209.pdf.html', '_blank');
-})
 
 // add event on nav-toggle-btn
 navToggleBtn.addEventListener('click', function () {
 
-  // If the shopping-cart has an `active` class, it will be removed.
-  if (shoppingCart.classList.contains('active')) cartToggleFunc();
 
   navToggleFunc();
 
 });
 
-// add event on cart-toggle-btn
-cartToggleBtn.addEventListener('click', function () {
-
-  // If the navbar-nav has an `active` class, it will be removed.
-  if (nav.classList.contains('active')) navToggleFunc();
-
-  cartToggleFunc();
-
-});
 
 // add event on all nav-link
 for (let i = 0; i < navLinks.length; i++) {
@@ -52,3 +33,11 @@ for (let i = 0; i < navLinks.length; i++) {
   navLinks[i].addEventListener('click', navToggleFunc);
 
 }
+
+const fullmenu = document.getElementById('fullmenu');
+fullmenu.addEventListener('click', function () {
+
+  console.log('clicked');
+  window.open('https://www.flipbookpdf.net/web/site/041f1a883dac6d1f9674fb42ca67e6ba5cced3e5202209.pdf.html', '_blank');
+})
+
